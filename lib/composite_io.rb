fa40c7ad100afc66d5ed31e96170a1fb3138b326
Greg Blass <greg@phillydevshop.com>
2022-08-25 12:08:24 -0400

    Silence warnings

--- lib/composite_io.rb
+++ lib/composite_io.rb
@@ -1,2 +1,2 @@
-warn "Top level ::CompositeIO is deprecated, require 'multipart/post' and use `Multipart::Post::CompositeReadIO` instead!"
+# warn "Top level ::CompositeIO is deprecated, require 'multipart/post' and use `Multipart::Post::CompositeReadIO` instead!"
 require_relative 'multipart/post'
