6aed48602e45a0eb187464b8167f7e9e47baaf97
Greg Blass <greg@phillydevshop.com>
2022-08-25 12:09:01 -0400

    Silence warning

--- lib/parts.rb
+++ lib/parts.rb
@@ -1,2 +1,2 @@
-warn "Top level ::Parts is deprecated, require 'multipart/post' and use `Multipart::Post::Parts` instead!"
+# warn "Top level ::Parts is deprecated, require 'multipart/post' and use `Multipart::Post::Parts` instead!"
 require_relative 'multipart/post'
